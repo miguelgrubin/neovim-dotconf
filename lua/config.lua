a8857c439bbf3dc522911b58c6757cdd3ef5414f
Miguel G. Rubin <miguelgr1988@gmail.com>
2021-11-23 11:39:55 +0100

    feat: prettier using local node_modules with JS and TS

--- lua/config.lua
+++ lua/config.lua
@@ -177,11 +177,23 @@ end
 
 M.neoformat = function()
   return function()
+    -- Enabled formatters
     vim.g.neoformat_enabled_python = { "black", "isort" }
     vim.g.neoformat_enabled_lua = { "stylua" }
     vim.g.neoformat_enabled_typescript = { "prettier" }
     vim.g.neoformat_enabled_javascript = { "prettier" }
     vim.g.neoformat_enabled_go = { "gofmt", "goimports" }
+    -- Local formatters
+    vim.g.neoformat_javascript_prettier = {
+      exe = "./node_modules/.bin/prettier",
+      args = { "--write", "--config .prettierrc" },
+      replace = 1,
+    }
+    vim.g.neoformat_typescript_prettier = {
+      exe = "./node_modules/.bin/prettier",
+      args = { "--write", "--config .prettierrc" },
+      replace = 1,
+    }
   end
 end
 
